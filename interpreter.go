5bdba035f47278cdf1a1c2d935be06fedbf5cf54
Joshua Crocker <joshua+code@crocker.io>
2021-01-31 10:07:44 +0000

    Refactor lexer code

--- interpreter.go
+++ interpreter.go
@@ -22,15 +22,44 @@ type interpreter struct {
 }
 
 type iinterpreter interface {
-	GetNextToken() token
-	Eat(tokenType string) error
+	getTokenParsers() []func(char string) token
+	getNextToken() token
+	eat(tokenType string) error
 	Parse() string
+}
+
+func (i *interpreter) getTokenParsers() []func(char string) token {
+	return []func(char string) token{
+		// Number
+		func(char string) token {
+			rDigit, _ := regexp.Compile("[0-9]")
+
+			if rDigit.MatchString(char) {
+				return token{number, char}
+			}
+
+			return token{}
+		},
+
+		// Plus
+		func(char string) token {
+			if char == "+" {
+				return token{plus, char}
+			}
+
+			return token{}
+		},
+	}
 }
 
 // GetNextToken from the input sentence
 //
-// Lexical analyser, breaks the input sentence down into tokens.
-func (i *interpreter) GetNextToken() (token, error) {
+// Lexical analyser, breaks the input sentence down into tokens by getting the
+// next character within the sentence, and iterates through the known parsers
+// to get the next token.
+//
+// Fails if no parser functions matches the given input.
+func (i *interpreter) getNextToken() (token, error) {
 	text := i.text
 
 	if i.pos >= len(text) {
@@ -38,25 +67,26 @@ func (i *interpreter) GetNextToken() (token, error) {
 	}
 
 	currentChar := string([]rune(text)[i.pos])
+	var t token
+	blankToken := token{}
 
-	rDigit, _ := regexp.Compile("[0-9]")
+	for _, parser := range i.getTokenParsers() {
+		t = parser(currentChar)
 
-	if rDigit.MatchString(currentChar) {
-		i.pos++
-		return token{number, currentChar}, nil
+		if t != blankToken {
+			i.pos++
+			return t, nil
+		}
 	}
 
-	if currentChar == "+" {
-		i.pos++
-		return token{plus, "+"}, nil
-	}
-
-	return token{}, errors.New("Undefined token")
+	return t, errors.New("Undefined token")
 }
 
-func (i *interpreter) Eat(tokenType string) error {
+// Eat the next token within the input. This method fails if the next token
+// is not of the expected type.
+func (i *interpreter) eat(tokenType string) error {
 	if i.currentToken.Type == tokenType {
-		next, err := i.GetNextToken()
+		next, err := i.getNextToken()
 		if nil != err {
 			panic(err)
 		}
@@ -69,26 +99,27 @@ func (i *interpreter) Eat(tokenType string) error {
 	return nil
 }
 
+// Parse an arithmetic expression
 func (i *interpreter) Parse() string {
 	// Set the current token to the first token
-	token, err := i.GetNextToken()
+	token, err := i.getNextToken()
 	failOnError(err)
 
 	i.currentToken = token
 
 	left := i.currentToken
-	err = i.Eat(number)
+	err = i.eat(number)
 	failOnError(err)
 
 	op := i.currentToken
-	err = i.Eat(plus)
+	err = i.eat(plus)
 	failOnError(err)
 
 	right := i.currentToken
-	err = i.Eat(number)
+	err = i.eat(number)
 	failOnError(err)
 
-	if op.Value == "+" {
+	if op.Type == plus {
 		leftInt, _ := strconv.Atoi(left.Value.(string))
 		rightInt, _ := strconv.Atoi(right.Value.(string))
 
